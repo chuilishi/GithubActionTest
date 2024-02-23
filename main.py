f5814a8f945558e29038219ca5f08cf6b3566657
chuilishi <108377487+chuilishi@users.noreply.github.com>
2024-02-23 21:29:32 +0800

    trjdfcjfyghvk

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-# 打开cache/test.txt 如果存在则打印exist,不存在则打印not exist 并且创建
 import os
-path = './cache/test.txt'
-# get current directory
-print(os.getcwd())
-if os.path.exists(path):
-    print('exist')
-else:
-    print('not exist')
-    with open(path, 'w') as f:
-        f.write('test')
+path = './cache/text.txt'
+# open in readable and writable
+with open(path,"r+") as f:
+    s = f.read()
+    i = int(s)
+    print(i)
+    # delete content in text.txt
+    f.seek(0)
+    f.truncate()
+    f.write(str(++i))
+    f.flush()
+    f.close()
