71c6dd487b2af4db5e313237d49840275132f9e9
chuilishi <108377487+chuilishi@users.noreply.github.com>
2024-02-23 21:13:54 +0800

    first

---
+++ main.py
@@ -0,0 +1,9 @@
+# 打开cache/test.txt 如果存在则打印exist,不存在则打印not exist 并且创建
+import os
+path = 'cache/test.txt'
+if os.path.exists(path):
+    print('exist')
+else:
+    print('not exist')
+    with open(path, 'w') as f:
+        f.write('test')
