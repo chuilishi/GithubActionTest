4f04805d8d7c9d9bc05af6d91f66bb9ab385c17a
chuilishi <108377487+chuilishi@users.noreply.github.com>
2024-02-23 21:21:45 +0800

    trjdfcjfyghvk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # 打开cache/test.txt 如果存在则打印exist,不存在则打印not exist 并且创建
 import os
-path = 'cache/test.txt'
+path = './cache/test.txt'
 # get current directory
 print(os.getcwd())
 if os.path.exists(path):
